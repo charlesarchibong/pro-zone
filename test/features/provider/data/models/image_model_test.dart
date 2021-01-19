82638ec68a2809c2ca94f3a35512fca570957276
charlesarchibong <charlesarchibong10@gmail.com>
2021-01-19 14:56:12 +0100

    wrote tests for the image model class

---
+++ test/features/provider/data/models/image_model_test.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:prozone_app/features/provider/data/models/image_model.dart';
+import 'package:prozone_app/features/provider/domain/entities/image_entity.dart';
+
+import '../../../../fixtures/fixture.dart';
+
+void main() {
+  ImageModel imageModel = ImageModel(
+    id: 1,
+    mediumImage: null,
+    alternativeText: "Test Image",
+    caption: "Test",
+    name: "image model",
+    smallImage: null,
+    thumbNail: null,
+  );
+  test('should test is ImageModel is a subclass of ImageEnity', () {
+    expect(imageModel, isA<ImageEntity>());
+  });
+
+  group("JsonSerializationTest", () {
+    test(
+        'should confirm if the image model was successfully converted from the json response',
+        () {
+      ImageModel imageModel = ImageModel.fromMap(
+        json.decode(
+          fixture('image_model'),
+        ),
+      );
+      print(imageModel.toJson());
+      expect(
+        imageModel,
+        isA<ImageModel>(),
+      );
+    });
+  });
+}
