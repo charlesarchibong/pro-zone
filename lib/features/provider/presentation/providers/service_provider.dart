0f4ceeb7ec5c2908f2db9f5f8024e3cc2bb9012c
charlesarchibong <charlesarchibong10@gmail.com>
2021-01-20 22:29:54 +0100

    get providers in state mgt

--- lib/features/provider/presentation/providers/service_provider.dart
+++ lib/features/provider/presentation/providers/service_provider.dart
@@ -1,4 +1,7 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
+import 'package:prozone_app/core/errors/failure.dart';
+import 'package:prozone_app/features/provider/domain/entities/provider_entity.dart';
 import 'package:prozone_app/features/provider/domain/use_cases/create_provider_usecase.dart';
 import 'package:prozone_app/features/provider/domain/use_cases/get_all_providers_usecase.dart';
 import 'package:prozone_app/features/provider/domain/use_cases/get_provider_types_usecase.dart';
@@ -21,11 +24,20 @@ class ServiceProvider extends ChangeNotifier {
   });
 
   bool _isHome = true;
-
   bool get isHome => _isHome;
+
+  List<ProviderEntity> _providers = List();
+  List<ProviderEntity> get providers => _providers;
 
   void toggleHome() {
     _isHome = !_isHome;
     notifyListeners();
+  }
+
+  Future<Either<Failure, List<ProviderEntity>>> getProvidersList() async {
+    final providers = await getAllProvidersUsecase();
+    providers.fold((l) => print(l), (r) => _providers = r);
+    notifyListeners();
+    return providers;
   }
 }
